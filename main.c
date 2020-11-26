49b2fb06a8a35831cedc657d40b27d15f5040885
Ilya Morozov <kvadrohan@mail.ru>
2020-11-26 20:48:39 +0300

    Final version

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 
 #include <windows.h>
+#include <string.h>
 
+#define bt1_id 1
+#define bt2_id 2
 
 #define AddEnemy(a,b) ObjectInit(NewObject(), a, b, 40, 40, 'e')
 
@@ -19,6 +22,9 @@ TPoint point (float x, float y)
 }
 
 TPoint cam;
+HWND hwnd;
+HWND bt1;
+HWND bt2;
 
 typedef struct SObject {
     TPoint pos;
@@ -55,7 +61,8 @@ void ObjectShow(TObject obj, HDC dc)
     SetDCBrushColor(dc, obj.brush);
 
     BOOL (*shape)(HDC, int, int, int, int);
-    shape = obj.oType == 'e' ? Ellipse : Rectangle;
+    //shape = obj.oType == 'e' ? Ellipse : Rectangle;
+    shape = Ellipse;
     shape(dc, (int)(obj.pos.x - cam.x), (int)(obj.pos.y - cam.y),
                     (int)(obj.pos.x + obj.size.x - cam.x), (int)(obj.pos.y + obj.size.y - cam.y));
 }
@@ -74,8 +81,10 @@ TObject player;
 
 PObject mas = NULL;
 int masCnt = 0;
+int score = 0;
 
 BOOL needNewGame = FALSE;
+BOOL startGame = FALSE;
 
 void SetCameraFocus(TObject obj)
 {
@@ -108,6 +117,7 @@ void ObjectMove(TObject *obj)
             {
                 mas[i].isDel = TRUE;
                 obj->isDel = TRUE;
+                score++;
             }
     }
 }
@@ -172,15 +182,50 @@ void WinInit()
 {
     needNewGame = FALSE;
     masCnt = 0;
+    score = 0;
     mas = realloc(mas, 0);
 
     ObjectInit(&player, 100, 100, 40, 40, 'p');
 }
 
+void ShowResult()
+{
+    char str[20];
+    str[0] = 'E';
+    str[1] = 'n';
+    str[2] = 'd';
+    str[3] = '!';
+    str[4] = ' ';
+    str[5] = 'S';
+    str[6] = 'c';
+    str[7] = 'o';
+    str[8] = 'r';
+    str[9] = 'e';
+    str[10] = ' ';
+    str[11] = '=';
+    str[12] = ' ';
+    str[13] = 0;
+    int radix = 10;
+    char buffer[20];
+    char *p;
+    p = itoa(score, buffer, radix);
+    MessageBox(hwnd, (LPCSTR)(strcat(str, buffer)), (LPCSTR)("End"), MB_OK);
+}
+
 void WinMove()
 {
     if (needNewGame)
+    {
+        ShowResult();
+        EnableWindow(bt1, TRUE);
+        EnableWindow(bt2, TRUE);
+        ShowWindow(bt1,SW_SHOW);
+        ShowWindow(bt2,SW_SHOW);
+
+        startGame = FALSE;
+
         WinInit();
+    }
 
     PlayerControl();
     ObjectMove(&player);
@@ -193,22 +238,45 @@ void WinMove()
     DelObjects();
 }
 
-void WinShow(HDC dc)
+void ShowBackground(HDC memDC)
 {
-    HDC memDC = CreateCompatibleDC(dc);
-    HBITMAP memBM = CreateCompatibleBitmap(dc, rct.right - rct.left, rct.bottom - rct.top);
-    SelectObject(memDC, memBM);
-
-    SelectObject(memDC, GetStockObject(DC_PEN));
-    SelectObject(memDC, GetStockObject(DC_BRUSH));
-    SetDCBrushColor(memDC, RGB(200, 200, 200));
-
     static int rectSize = 200;
     int dx = (int)(cam.x) % rectSize;
     int dy = (int)(cam.y) % rectSize;
     for (int i = -1; i < (rct.right / rectSize) + 2; i++)
         for (int j = -1; j < (rct.bottom / rectSize) + 2; j++)
             Rectangle(memDC, -dx+(i*rectSize), -dy+(j*rectSize), -dx+((i+1)*rectSize), -dy+((j+1)*rectSize));
+}
+
+void WinShow(HDC dc)
+{
+    HDC memDC = CreateCompatibleDC(dc);
+    HBITMAP memBM = CreateCompatibleBitmap(dc, rct.right - rct.left, rct.bottom - rct.top);
+    //HBITMAP memBM = (HBITMAP)LoadImage(NULL, L"D:\\aaa.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    //memBM = CreateCompatibleBitmap(dc, rct.right - rct.left, rct.bottom - rct.top);
+    SelectObject(memDC, memBM);
+
+    SelectObject(memDC, GetStockObject(DC_PEN));
+    SelectObject(memDC, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(memDC, RGB(200, 200, 200));
+
+    ShowBackground(memDC);
+
+    char str[20];
+    str[0] = 'S';
+    str[1] = 'c';
+    str[2] = 'o';
+    str[3] = 'r';
+    str[4] = 'e';
+    str[5] = ':';
+    str[6] = 0;
+    int radix = 10;
+    char buffer[20];
+    char *p;
+    p = itoa(score, buffer, radix);
+
+    TextOutA(memDC, 10, 10, (LPCSTR)(strcat(str, buffer)), lstrlen(strcat(str, buffer)));
+
 
     ObjectShow(player, memDC);
 
@@ -241,6 +309,17 @@ LRESULT  WndProc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
         GetClientRect(hWnd, &rct);
     }
+    else if (message == WM_COMMAND)
+    {
+        if (LOWORD(wParam) == bt1_id)
+        {
+            PostQuitMessage(0);
+        }
+        if (LOWORD(wParam) == bt2_id)
+        {
+            startGame = TRUE;
+        }
+    }
 
     else return DefWindowProcA( hWnd,  message,  wParam,  lParam);
 }
@@ -254,13 +333,20 @@ int main()
         wcl.hCursor = LoadCursorA(NULL, IDC_CROSS);
     RegisterClassA(&wcl);
 
-    HWND hwnd;
-    hwnd = CreateWindow("My window", "Window name", WS_OVERLAPPEDWINDOW,
+    hwnd = CreateWindow("My window", "My best game", WS_OVERLAPPEDWINDOW&~WS_THICKFRAME,
                          10, 10, 640, 480, NULL, NULL, NULL, NULL);
 
     HDC dc = GetDC(hwnd);
 
     ShowWindow(hwnd, SW_SHOWNORMAL);
+
+    ShowBackground(dc);
+
+    bt1 = CreateWindow("button", "Quit", WS_VISIBLE | WS_CHILD,
+                            250, 220, 100, 50, hwnd, bt1_id, NULL, NULL);
+
+    bt2 = CreateWindow("button", "Start", WS_VISIBLE | WS_CHILD,
+                            250, 140, 100, 50, hwnd, bt2_id, NULL, NULL);
 
     WinInit();
 
@@ -275,9 +361,15 @@ int main()
         }
         else
         {
-            WinMove();
-            WinShow(dc);
-            Sleep(5);
+             if (startGame){
+                EnableWindow(bt1, FALSE);
+                EnableWindow(bt2, FALSE);
+                ShowWindow(bt1,SW_HIDE);
+                ShowWindow(bt2,SW_HIDE);
+                WinMove();
+                WinShow(dc);
+                Sleep(5);
+             }
         }
     }
     printf("Hello world!\n");
